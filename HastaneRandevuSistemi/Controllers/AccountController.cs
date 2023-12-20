ae61869e107393687726c15f2360126303048495
Abdulkadir_USTA <usta1829@gmail.com>
2023-12-20 23:47:47 +0300

    Add project files.

---
+++ HastaneRandevuSistemi/Controllers/AccountController.cs
@@ -0,0 +1,23 @@
+﻿using HastaneRandevuSistemi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HastaneRandevuSistemi.Controllers
+{
+    public class AccountController : Controller
+    {
+        
+        public IActionResult Login(LoginViewModel model)
+        {
+            if(ModelState.IsValid)
+            {
+                // login işlemleri
+            }
+            return View(model);
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
---
+++ HastaneRandevuSistemi/Models/LoginViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HastaneRandevuSistemi.Models
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage ="isim boş bırakılamaz")]
+        [StringLength(30, ErrorMessage ="isim 30'dan büyük olamaz")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "şifre boş bırakılamaz")]
+        [MinLength(6, ErrorMessage ="en az 6 karakter olmalı")]
+        [MaxLength(16, ErrorMessage ="en fazla 16 karakter olabilir")]
+        public string Password { get; set; }  
+        public string Email { get; set; }
+    }
+}
