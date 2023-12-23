869f862b064bf360857eb0897a8d79ee0c7d2fb6
Abdulkadir_USTA <usta1829@gmail.com>
2023-12-23 23:00:13 +0300

    Doktor Ekleme sayfası hazırlandı.

---
+++ HastaneRandevuSistemi/Controllers/DoctorController.cs
@@ -0,0 +1,26 @@
+﻿using HastaneRandevuSistemi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HastaneRandevuSistemi.Controllers
+{
+    public class DoctorController : Controller
+    {
+        
+        public IActionResult Ekle()
+        {
+            return View();
+        }
+        /*
+        [HttpPost]
+        public IActionResult Ekle(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                // ekleme işlemleri yapılacak
+            }
+            return View();
+        }*/
+
+
+    }
+}
--- HastaneRandevuSistemi/Models/LoginViewModel.cs
+++ HastaneRandevuSistemi/Models/LoginViewModel.cs
@@ -12,7 +12,7 @@ namespace HastaneRandevuSistemi.Models
         [Required(ErrorMessage = "şifre boş bırakılamaz")]
         [MinLength(6, ErrorMessage ="en az 6 karakter olmalı")]
         [MaxLength(25, ErrorMessage ="en fazla 25 karakter olabilir")]
-        public string Password { get; set; }  
+        public string? Password { get; set; }  
         public string? Email { get; set; }
     }
 }
